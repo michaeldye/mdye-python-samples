3a7524d1395b2a50cb82c009c271f2d14dfa3c08
michael dye <m-github@divisive.info>
2022-09-13 21:30:31 -0600

    add solution 300, lis

---
+++ src/mdye_leetcode/solution_300_longest_increasing_subsequence.py
@@ -0,0 +1,31 @@
+# -*- coding: latin-1 -*-
+
+# need a structure that allows low cost prepending; deque is good, so is linked list
+from collections import deque
+
+from typing import List
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        return len(self.patienceSort(nums))
+
+    def patienceSort(self, nums: List[int]) -> List[List[int]]:
+
+        piles: List[List[int]] = []
+
+        for n in nums:
+            written = False
+
+            # optimize by using binary search on head elements in all piles instead of iterating over them
+            for p in piles:
+                if len(p) > 0 and p[0] >= n:
+                    written = True
+                    p.appendleft(n) # prepend
+                    break # only write number to one pile
+
+            if not written:
+                piles.append(deque([n]))
+
+        return piles
+
+# vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4
---
+++ src/mdye_leetcode/test/test_solution_300.py
@@ -0,0 +1,14 @@
+import pytest
+
+from mdye_leetcode.solution_300_longest_increasing_subsequence import Solution
+
+# makes a Solution object b/c that's how leetcode rolls
+@pytest.fixture(scope="module")
+def sol():
+    yield Solution()
+
+
+def test_solution_300_basic(sol: Solution):
+    assert 4 == sol.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]) # 2, 3, 7, 101 is longest increasing
+
+# vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4
