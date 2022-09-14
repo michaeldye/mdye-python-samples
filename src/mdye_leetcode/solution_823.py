7df419da302407be3399eb359d1fb0502ee8a503
michael dye <m-github@divisive.info>
2022-09-13 18:24:35 -0600

    improved documentation and tests for 823

--- src/mdye_leetcode/solution_823.py
+++ src/mdye_leetcode/solution_823.py
@@ -10,19 +10,29 @@ class Solution:
         dp = {x: 1 for x in arr}
 
         for ix, a in enumerate(arr):
-            for b in arr[0:ix]:  # sliding window from 0 up to ix
+            for b in arr[0:ix]:
 
-                # (a, b) is a candidate pair of factors
+                # (a, b) is a candidate pair of factors; for input (after sorting) [2,4,5,10], the first pair to get here will be a=4,b=2 , then a=5,b=2 , then a=5,b=4 ...
 
                 if a % b == 0:
-                    # b divides a evenly so we count it; the key into our dp map is int(a / b)
-                    key = a // b
-                    if key in dp:
-                        # we found a factor that is a node in our dp map, multiply the recorded ct for that node by the recorded ct for the divisor, b. Why ???
-                        dp[a] += dp[b] * dp[key]
-        from pprint import pprint
+                    # b divides a evenly so next we need to see if the other factor ("c", where c * b == a)
+                    # is in our list of nodes. If both "c" and "b" are in our list of nodes we can make a
+                    # binary tree where "a" is the parent and "c" and "b" are children, just as the problem
+                    # requires.
+                    #
+                    # The best way to see if c is in the list of nodes is to use the dict we built earlier.
 
-        pprint(dp)
+                    c = a // b
+                    if c in dp:
+                        # We've found a tree of "a" (and its mirror will be picked up when we get to the
+                        # other factor in our iteration). But if we were to only increment dp[a] by 1 here,
+                        # we would fail to count all of the subtrees in that tree and those are valid too
+                        # (so not just 18 with children 6 and 3, and its mirror, but also 18 with children
+                        # 6 and 3, where 6 has children 2 and 3, and then all variations of those). The
+                        # way to get all variations of the subtrees is to multiply the counts we have
+                        # so far
+                        #
+                        dp[a] += dp[b] * dp[c]
 
         # sum of all counts recorded in dp map mod the special num they gave us in the instructions
         return sum(dp.values()) % (10**9 + 7)
--- src/mdye_leetcode/test/test_solution_823.py
+++ src/mdye_leetcode/test/test_solution_823.py
@@ -9,8 +9,13 @@ def sol():
 
 
 def test_solution_823_basic(sol: Solution):
+    # these are all mini trees (where each is just a parent and two children)
     assert 3 == sol.numFactoredBinaryTrees([2, 4])
     assert 7 == sol.numFactoredBinaryTrees([2, 4, 5, 10])
+
+def test_solution_823_deep(sol: Solution):
+    # these are bigger trees than the minis and the solution needs to count all of the subtrees, including mirrors
+    assert 12 == sol.numFactoredBinaryTrees([18, 3, 6, 2])
 
 
 # vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4
