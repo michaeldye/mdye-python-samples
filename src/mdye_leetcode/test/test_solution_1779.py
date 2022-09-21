c97be8e29c756388e0f055099177f488a67d180b
michael dye <m-github@divisive.info>
2022-09-20 23:03:09 -0600

    add leetcode 1779

---
+++ src/mdye_leetcode/test/test_solution_1779.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from mdye_leetcode.solution_1779 import Solution
+
+
+# makes a Solution object b/c that's how leetcode rolls
+@pytest.fixture(scope="module")
+def sol():
+    yield Solution()
+
+
+def test_solution_1779_basic(sol: Solution):
+    assert 2 == sol.nearestValidPoint(3, 4, [[1, 2], [3, 1], [2, 4], [2, 3], [4, 4]])
+
+# vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4
