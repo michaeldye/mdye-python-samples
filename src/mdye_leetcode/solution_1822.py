8e5c4146d6cffe14bf54882b4bad595e79ef6ce1
michael dye <m-github@divisive.info>
2022-09-23 10:15:54 -0600

    solved leetcode 1822

---
+++ src/mdye_leetcode/solution_1822.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Solution:
+
+    def arraySign(self, nums: List[int]) -> int:
+
+        neg_ct = 0
+        for n in nums:
+
+            if n == 0:
+                return 0
+            if n * -1 > 0:
+                # n is negative
+                neg_ct += 1
+
+        if neg_ct % 2 == 0:
+            # return 1 if positive
+            return 1
+        else:
+            # return -1 if negative
+            return -1
+
+# vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4
