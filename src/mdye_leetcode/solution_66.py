9f96a4897a2ee8ebff2200860c18b81925c1754c
michael dye <m-github@divisive.info>
2022-09-30 23:08:38 -0600

    added leetcode 66

---
+++ src/mdye_leetcode/solution_66.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        v = 0
+        for d in digits:
+            v *= 10
+            v += d
+
+        v += 1
+
+        o = deque()
+        while v > 0:
+            o.appendleft(v % 10)
+            v //= 10
+
+        return list(o)
+
+
+# vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4
