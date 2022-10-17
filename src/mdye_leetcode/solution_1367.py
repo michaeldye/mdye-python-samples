9574c0677d68072b06fe9f5e6065b21a82eed735
michael dye <m-github@divisive.info>
2022-10-16 21:17:30 -0600

    add 1367

--- src/mdye_leetcode/solution_1367.py
+++ src/mdye_leetcode/solution_1367.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from typing import Optional
 
 
@@ -8,6 +7,9 @@ class ListNode:
         self.val = val
         self.next = next
 
+    def __str__(self) -> str:
+        return f"{self.val=}, {self.next=}"
+
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -15,21 +17,36 @@ class TreeNode:
         self.left = left
         self.right = right
 
+    def __str__(self) -> str:
+        return f"{self.val=}, {self.left=}, {self.right=}"
+
 
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        if not head:
+        def dfs(list_n: Optional[ListNode], n: Optional[TreeNode]) -> bool:
+            if list_n is None:
+                return True
+
+            if n is None:
+                return False
+
+            return list_n.val == n.val and (
+                dfs(list_n.next, n.left) or dfs(list_n.next, n.right)
+            )
+
+        if head is None:
             return True
 
-        if not root:
+        if root is None:
             return False
 
-        if head.val == root.val:
-            rest = head.next
-        else:
-            rest = head
-
-        return self.isSubPath(rest, root.left) or self.isSubPath(rest, root.right)
+        # At each node, do depth-first-search attempting to match each node in
+        # the list; if no match is found, recur down each branch of the tree
+        return (
+            dfs(head, root)
+            or self.isSubPath(head, root.left)
+            or self.isSubPath(head, root.right)
+        )
 
 
 # vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4
